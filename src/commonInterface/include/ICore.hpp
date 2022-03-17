a963aa7459ec8956e31bbcc5614f20e9db8e580e
Cavonstavant <constant.vigneron@protonmail.com>
2022-03-17 01:25:07 +0100

    [*] Added basic IGame methods definitions

---
+++ src/commonInterface/include/ICore.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** arcade
+** File description:
+** ICore
+*/
+
+#ifndef ARCADE_ICORE_HPP
+#define ARCADE_ICORE_HPP
+
+
+class ICore {
+};
+
+
+#endif//ARCADE_ICORE_HPP

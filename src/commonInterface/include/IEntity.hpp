88d46d10ddaf72c56af7e01b866de420f28f97b3
Restyled.io <commits@restyled.io>
2022-03-17 17:19:27 +0000

    Restyled by clang-format

--- src/commonInterface/include/IEntity.hpp
+++ src/commonInterface/include/IEntity.hpp
@@ -11,23 +11,23 @@
 #include <string>
 
 class IEntity {
-public:
-    enum EntityType {
-        PLAYER_ENTITY,
-        ENEMY_ENTITY,
-        ITEM_ENTITY,
-        WALL_ENTITY,
-    };
+    public:
+        enum EntityType {
+            PLAYER_ENTITY,
+            ENEMY_ENTITY,
+            ITEM_ENTITY,
+            WALL_ENTITY,
+        };
 
-    ~IEntity() = default;
-    virtual std::string getTexture() const = 0;
-    virtual void setTexture(const std::string &texture) = 0;
-    virtual EntityType getType() const = 0;
-    virtual void setType(EntityType type) = 0;
-    virtual std::pair<int, int> getPos() const = 0;
-    virtual void setPos(std::pair<int, int> pos) = 0;
-    virtual std::pair<int, int> getSize() const = 0;
-    virtual void setSize(std::pair<int, int> size) = 0;
+        ~IEntity() = default;
+        virtual std::string getTexture() const = 0;
+        virtual void setTexture(const std::string &texture) = 0;
+        virtual EntityType getType() const = 0;
+        virtual void setType(EntityType type) = 0;
+        virtual std::pair<int, int> getPos() const = 0;
+        virtual void setPos(std::pair<int, int> pos) = 0;
+        virtual std::pair<int, int> getSize() const = 0;
+        virtual void setSize(std::pair<int, int> size) = 0;
 };
 
 #endif /* !IENTITY_HPP_ */

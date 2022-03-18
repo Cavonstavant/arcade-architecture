0193d62b74f04c4f889dec6bf8b9e7fbbec054d6
Cavonstavant <constant.vigneron@protonmail.com>
2022-03-18 11:48:11 +0100

    Squashed 'Src/CommomInterface/' content from commit 019eacc
    git-subtree-dir: Src/CommomInterface
    git-subtree-split: 019eacc3f75a1c236e3ab1ec550810f7d816b635

---
+++ include/IDisplay.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** arcade
+** File description:
+** IDisplay
+*/
+
+#ifndef ARCADE_IDISPLAY_HPP
+#define ARCADE_IDISPLAY_HPP
+
+
+class IDisplay {
+};
+
+
+#endif//ARCADE_IDISPLAY_HPP

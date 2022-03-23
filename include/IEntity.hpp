d11056bf22eb3acf4088fe717a3ce119015272f4
Cavonstavant <constant.vigneron@protonmail.com>
2022-03-23 11:16:05 +0100

    [*] Update CMakeLists.txt

--- include/IEntity.hpp
+++ include/IEntity.hpp
@@ -10,34 +10,37 @@
 
 #include <string>
 
+/// \brief represents a generic entity within the game
 class IEntity {
     public:
+        /// \brief represent the entity type
         enum EntityType {
-            PLAYER_ENTITY,
-            ENEMY_ENTITY,
-            ITEM_ENTITY,
-            WALL_ENTITY,
+            PLAYER,
+            ENEMY,
+            ITEM,
+            WALL,
+            ENTITY_NUM //< Keep last to track the number of type avail
         };
 
-        /// @brief Destructor
+        /// \brief Destructor
         ~IEntity() = default;
-        /// @brief Get the entity type
-        /// @return the EntityType
+        /// \brief Get the entity type
+        /// \return the EntityType
         virtual EntityType getType() const = 0;
-        /// @brief Set the entity type
-        /// @param type the EntityType
+        /// \brief Set the entity type
+        /// \param type the EntityType
         virtual void setType(EntityType type) = 0;
-        /// @brief Get the entity position
-        /// @return the std::pair<int, int> position
+        /// \brief Get the entity position
+        /// \return the std::pair<int, int> position
         virtual std::pair<int, int> getPos() const = 0;
-        /// @brief Set the entity position
-        /// @param pos the std::pair<int, int> position
+        /// \brief Set the entity position
+        /// \param pos the std::pair<int, int> position
         virtual void setPos(std::pair<int, int> pos) = 0;
-        /// @brief Get the entity size
-        /// @return the std::pair<int, int> size
+        /// \brief Get the entity size
+        /// \return the std::pair<int, int> size
         virtual std::pair<int, int> getSize() const = 0;
-        /// @brief Set the entity size
-        /// @param size the std::pair<int, int> size
+        /// \brief Set the entity size
+        /// \param size the std::pair<int, int> size
         virtual void setSize(std::pair<int, int> size) = 0;
 };
 

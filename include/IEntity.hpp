830d070b86e33a4565658e45a80e62fb12b36dfb
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-03-24 17:32:20 +0100

    [*] Delete Entity type, creating classes instead

--- include/IEntity.hpp
+++ include/IEntity.hpp
@@ -13,25 +13,8 @@
 /// \brief represents a generic entity within the game
 class IEntity {
     public:
-        /// \brief represent the entity type
-        enum EntityType {
-            PLAYER,
-            ENEMY,
-            ITEM,
-            WALL,
-            ENTITY_NUM //< Keep last to track the number of type avail
-        };
-
         /// \brief Destructor
         ~IEntity() = default;
-        /// \brief Get the entity type
-        /// \return the EntityType
-        virtual EntityType getType() const = 0;
-        /// \brief Set the entity type
-        /// \param type the EntityType
-        virtual void setType(EntityType type) = 0;
-        /// \brief Get the entity position
-        /// \return the std::pair<int, int> position
         virtual std::pair<int, int> getPos() const = 0;
         /// \brief Set the entity position
         /// \param pos the std::pair<int, int> position

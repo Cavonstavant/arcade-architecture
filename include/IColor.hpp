5d186ef755a046bf64c1122fe51c70b7c90d237a
Restyled.io <commits@restyled.io>
2022-03-24 16:35:27 +0000

    Restyled by astyle

--- include/IColor.hpp
+++ include/IColor.hpp
@@ -10,25 +10,25 @@
 
 /// \brief Color interface for entities.
 class IColor {
-    public:
-        /// \brief Enum of standard colors
-        enum COLOR_E{
-            RED,
-            YELLOW,
-            BLUE,
-            ORANGE
-        };
-        /// \brief Destructor
-        virtual ~IColor() = default;
-        /// \brief Getter & Setter
-        /// \brief Set the color
-        /// \param COLOR_E color
-        virtual void setColor(const COLOR_E color) = 0;
-        /// \brief Get the color
-        /// \return IColor
-        virtual IColor getColor() const = 0;
-    protected:
-    private:
+public:
+    /// \brief Enum of standard colors
+    enum COLOR_E {
+        RED,
+        YELLOW,
+        BLUE,
+        ORANGE
+    };
+    /// \brief Destructor
+    virtual ~IColor() = default;
+    /// \brief Getter & Setter
+    /// \brief Set the color
+    /// \param COLOR_E color
+    virtual void setColor(const COLOR_E color) = 0;
+    /// \brief Get the color
+    /// \return IColor
+    virtual IColor getColor() const = 0;
+protected:
+private:
 };
 
 
--- include/IEntity.hpp
+++ include/IEntity.hpp
@@ -12,19 +12,19 @@
 
 /// \brief represents a generic entity within the game
 class IEntity {
-    public:
-        /// \brief Destructor
-        ~IEntity() = default;
-        virtual std::pair<int, int> getPos() const = 0;
-        /// \brief Set the entity position
-        /// \param pos the std::pair<int, int> position
-        virtual void setPos(std::pair<int, int> pos) = 0;
-        /// \brief Get the entity size
-        /// \return the std::pair<int, int> size
-        virtual std::pair<int, int> getSize() const = 0;
-        /// \brief Set the entity size
-        /// \param size the std::pair<int, int> size
-        virtual void setSize(std::pair<int, int> size) = 0;
+public:
+    /// \brief Destructor
+    ~IEntity() = default;
+    virtual std::pair<int, int> getPos() const = 0;
+    /// \brief Set the entity position
+    /// \param pos the std::pair<int, int> position
+    virtual void setPos(std::pair<int, int> pos) = 0;
+    /// \brief Get the entity size
+    /// \return the std::pair<int, int> size
+    virtual std::pair<int, int> getSize() const = 0;
+    /// \brief Set the entity size
+    /// \param size the std::pair<int, int> size
+    virtual void setSize(std::pair<int, int> size) = 0;
 };
 
 #endif /* !IENTITY_HPP_ */

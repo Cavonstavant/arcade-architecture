cdb2425ebbd1b01b566db4f21b582f1e02549c13
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-02 21:31:35 +0200

    [+] add Documentation of color structure

--- include/Color.hpp
+++ include/Color.hpp
@@ -8,22 +8,25 @@
 #ifndef ARCADE_COLOR_HPP
 #define ARCADE_COLOR_HPP
 
-#include <cstddef>
-
 /// \brief Color structure
 struct Color {
+    /// \brief Default Constructor
     Color() = default;
-    Color(char r, char g, char b, char a) {
-        _r = r;
-        _g = g;
-        _b = b;
-        _a = a;
-    };
+    /// \brief Constructor with R, G, B, A params
+    /// \param R Amount of Red on a scale of 0 to 255
+    /// \param G Amount of Green on a scale of 0 to 255
+    /// \param B Amount of Blue on a scale of 0 to 255
+    /// \param A Amount of Alpha of the color on a scale of 0 to 255. It represents the opacity of the color
+    Color(int r, int g, int b, int a) : R(r), G(g), B(b), A(a){};
     ~Color() = default;
-    int _r;
-    int _g;
-    int _b;
-    int _a;
+    /// \brief Red amount on a scale of 0 to 255
+    int R;
+    /// \brief Green amount on a scale of 0 to 255
+    int G;
+    /// \brief Blue amount on a scale of 0 to 255
+    int B;
+    /// \brief Alpha amount on a scale of 0 to 255
+    int A;
 };
 
 

0724f47c09477d5ae82071e5f718494159de2959
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-02 21:37:50 +0200

    [*] Completing missing Documentation

--- include/Color.hpp
+++ include/Color.hpp
@@ -10,15 +10,20 @@
 
 /// \brief Color structure
 struct Color {
+
     /// \brief Default Constructor
     Color() = default;
+
     /// \brief Constructor with R, G, B, A params
     /// \param R Amount of Red on a scale of 0 to 255
     /// \param G Amount of Green on a scale of 0 to 255
     /// \param B Amount of Blue on a scale of 0 to 255
     /// \param A Amount of Alpha of the color on a scale of 0 to 255. It represents the opacity of the color
     Color(int r, int g, int b, int a) : R(r), G(g), B(b), A(a){};
+
+    /// \brief Default Destructor
     ~Color() = default;
+
     /// \brief Red amount on a scale of 0 to 255
     int R;
     /// \brief Green amount on a scale of 0 to 255

140dc2656bf27852468d4c61ce271c17bef3aa3d
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-02 18:43:07 +0200

    [+] Color.hpp structure

---
+++ include/Color.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2022
+** Color.hpp.h
+** File description:
+** Created by hjulien,
+*/
+
+#ifndef ARCADE_COLOR_HPP
+#define ARCADE_COLOR_HPP
+
+#include <cstddef>
+
+/// \brief Color structure
+struct Color {
+    Color() = default;
+    Color(char r, char g, char b, char a) {
+        _r = r;
+        _g = g;
+        _b = b;
+        _a = a;
+    };
+    ~Color() = default;
+    int _r;
+    int _g;
+    int _b;
+    int _a;
+};
+
+
+#endif//ARCADE_COLOR_HPP

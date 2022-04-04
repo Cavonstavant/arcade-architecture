7d86d3da3cba3e8229f31c5d757fe85215dbc75b
Restyled.io <commits@restyled.io>
2022-04-04 23:44:49 +0000

    Restyled by clang-format

--- include/IGame.hpp
+++ include/IGame.hpp
@@ -18,32 +18,32 @@
 
 /// Game instance
 class IGame {
-public:
-    /// Represents the game's status
-    enum class GameState {
-        /// Represents the game at it's initial state
-        LOADED,
-        /// Represents the game when running
-        RUNNING,
-        /// Represents the previously running game is paused
-        PAUSED,
-        /// Represents the game over
-        STOPPED,
-    };
+    public:
+        /// Represents the game's status
+        enum class GameState {
+            /// Represents the game at it's initial state
+            LOADED,
+            /// Represents the game when running
+            RUNNING,
+            /// Represents the previously running game is paused
+            PAUSED,
+            /// Represents the game over
+            STOPPED,
+        };
 
-    virtual ~IGame() = default;
+        virtual ~IGame() = default;
 
-    /// \brief Update the game's status
-    /// \param entities The entity vector of the game
-    /// \param events The event stack of the game
-    virtual void update(std::vector<std::shared_ptr<IEntity>> &entities, std::stack<Arcade::Evt> &events) = 0;
+        /// \brief Update the game's status
+        /// \param entities The entity vector of the game
+        /// \param events The event stack of the game
+        virtual void update(std::vector<std::shared_ptr<IEntity>> &entities, std::stack<Arcade::Evt> &events) = 0;
 
-    /// \brief Get the game's status
-    /// \return the game's status
-    virtual GameState getState() const = 0;
+        /// \brief Get the game's status
+        /// \return the game's status
+        virtual GameState getState() const = 0;
 
-    /// \brief starts the game
-    virtual void start() = 0;
+        /// \brief starts the game
+        virtual void start() = 0;
 };
 
 

6874b2f17f111d5523534b67c89339168fb7fb77
aurelenc <aurele.nicolas@epitech.eu>
2022-04-05 01:27:58 +0200

    [+] Added start of the main menu with a button to close the game

--- include/Event.hpp
+++ include/Event.hpp
@@ -10,86 +10,86 @@
 #ifndef ARCADECOMMONINTERFACE_EVENT_HPP
 #define ARCADECOMMONINTERFACE_EVENT_HPP
 
-namespace arcade {
+namespace Arcade {
 
-/// \brief Represents a generic event interface
-/// this interface is shared across all the graphical libraries
-struct Evt {
+  /// \brief Represents a generic event interface
+  /// this interface is shared across all the graphical libraries
+  struct Evt {
 
-  /// \brief data associated with a keyboard event
-  /// currently supports only keycode from ascii table
-  typedef char Key;
+    /// \brief data associated with a keyboard event
+    /// currently supports only keycode from ascii table
+    typedef char Key;
 
-  /// \brief represents the state of a key (pressed or released)
-  /// this is used is mouse click events
-  typedef bool State;
+    /// \brief represents the state of a key (pressed or released)
+    /// this is used is mouse click events
+    typedef bool State;
 
-  /// \brief represents which mouse button was pressed
-  enum MouseClick { LEFT, RIGHT, MIDDLE, MOUSE_CLICK_SIZE };
+    /// \brief represents which mouse button was pressed
+    enum MouseClick { LEFT, RIGHT, MIDDLE, MOUSE_CLICK_SIZE };
 
-  /// \brief represents the position of the cursor
-  /// \todo x, y should be a templated vec2i
-  template <typename T> struct MouseMove {
-    T x;
-    T y;
+    /// \brief represents the position of the cursor
+    /// \todo x, y should be a templated vec2i
+    template <typename T> struct MouseMove {
+      T x;
+      T y;
+    };
+
+    /// \brief represents the direction of the mouse wheel scoll
+    enum MouseWheel { UP, DOWN, MOUSE_WHEEL_SIZE };
+
+    /// \brief represents the mouse event parameters
+    template <typename T> struct MouseEvt {
+      T x;
+      T y;
+      State state;
+      MouseClick click;
+      MouseWheel wheel;
+    };
+
+    /// \brief represents a key modifier (ctrl, shift, alt, etc.)
+    enum KeyModifier {
+      KEY_MODIFIER_NONE,
+      KEY_MODIFIER_SHIFT,
+      KEY_MODIFIER_CTRL,
+      KEY_MODIFIER_ALT,
+      KEY_MODIFIER_SIZE
+    };
+
+    /// \brief represents a key event parameters
+    struct KeyEvt {
+      Key key;
+      State state;
+      KeyModifier modifier;
+    };
+
+    /// \brief represents a the resize event parameters
+    template <typename T> struct ResizeEvt {
+      T oldSize;
+      T newSize;
+    };
+    /// \brief represents each type of event handled by the core
+    enum EvtType {
+      WIN_CLOSE,   //< window close event, no data
+      WIN_RESIZE,  //< window resize event, data in ResizeEvt
+      KEY,         //< key event, data in KeyEvt
+      MOUSE_MOVE,  //< mouse move event, data in MouseEvt::(x, y)
+      MOUSE_CLICK, //< mouse click event, data in MouseEvt::MouseClick
+      MOUSE_WHEEL, //< mouse wheel event, data in MouseEvt::MouseWheel
+      EVT_TYPE_SIZE
+    };
+
+    /// \brief event type
+    EvtType evt_type;
+
+    /// \brief event data
+    union {
+      /// \brief key event data, see KeyEvt
+      KeyEvt key;
+      /// \brief mouse event data, see MouseEvt
+      MouseEvt<float> mouse;
+      /// \brief resize event data, see ResizeEvt
+      ResizeEvt<float> resize;
+    };
   };
-
-  /// \brief represents the direction of the mouse wheel scoll
-  enum MouseWheel { UP, DOWN, MOUSE_WHEEL_SIZE };
-
-  /// \brief represents the mouse event parameters
-  template <typename T> struct MouseEvt {
-    T x;
-    T y;
-    State state;
-    MouseClick click;
-    MouseWheel wheel;
-  };
-
-  /// \brief represents a key modifier (ctrl, shift, alt, etc.)
-  enum KeyModifier {
-    KEY_MODIFIER_NONE,
-    KEY_MODIFIER_SHIFT,
-    KEY_MODIFIER_CTRL,
-    KEY_MODIFIER_ALT,
-    KEY_MODIFIER_SIZE
-  };
-
-  /// \brief represents a key event parameters
-  struct KeyEvt {
-    Key key;
-    State state;
-    KeyModifier modifier;
-  };
-
-  /// \brief represents a the resize event parameters
-  template <typename T> struct ResizeEvt {
-    T oldSize;
-    T newSize;
-  };
-  /// \brief represents each type of event handled by the core
-  enum EvtType {
-    WIN_CLOSE,   //< window close event, no data
-    WIN_RESIZE,  //< window resize event, data in ResizeEvt
-    KEY,         //< key event, data in KeyEvt
-    MOUSE_MOVE,  //< mouse move event, data in MouseEvt::(x, y)
-    MOUSE_CLICK, //< mouse click event, data in MouseEvt::MouseClick
-    MOUSE_WHEEL, //< mouse wheel event, data in MouseEvt::MouseWheel
-    EVT_TYPE_SIZE
-  };
-
-  /// \brief event type
-  EvtType evt_type;
-
-  /// \brief event data
-  union {
-    /// \brief key event data, see KeyEvt
-    KeyEvt key;
-    /// \brief mouse event data, see MouseEvt
-    MouseEvt<float> mouse;
-    /// \brief resize event data, see ResizeEvt
-    ResizeEvt<float> resize;
-  };
-};
-} // namespace arcade
+} // namespace Arcade
 #endif // ARCADECOMMONINTERFACE_EVENT_HPP
--- include/IGame.hpp
+++ include/IGame.hpp
@@ -11,6 +11,10 @@
 #define ARCADE_IGAME_HPP
 
 #include "IGraph.hpp"
+#include <vector>
+#include <memory>
+#include <stack>
+#include "Event.hpp"
 
 /// Game instance
 class IGame {
@@ -26,42 +30,20 @@ class IGame {
             /// Represents the game over
             STOPPED,
         };
-        IGame() = default;
 
         virtual ~IGame() = default;
 
-        /// \brief Initialize a game instance
-        /// \param display a display instance to use
-        /// \return true if the game was successfully initialized
-        /// \note Each call to the method must be followed by a call to IGame::destroy()
-        /// \warning Calling this method twice without calling IGame::destroy() beforehand will result in <b>undefined behavior</b>
-        virtual bool setup(IGraph &display) = 0;
-
-        /// \brief Destroy a game instance
-        /// \return true if the game was successfully destroyed
-        /// \note Calling this method twice without calling IGame::setup() beforehand will result in <b>undefined behavior</b>
-        virtual bool destroy() = 0;
-
-        /// \brief Update the game's display
-        /// \return true if the game was successfully updated
-        /// \note Calling this method without calling IGame::setup() beforehand will result in <b>undefined behavior</b>
-        virtual bool setDisplay(IGraph &display) = 0;
-
         /// \brief Update the game's status
-        /// \param state the new game's status
-        /// \return true if the game was successfully updated
-        /// \note Calling this method without calling IGame::setup() beforehand will result in <b>undefined behavior</b>
-        virtual bool update(GameState state) = 0;
+        /// \param entities The entity vector of the game
+        /// \param events The event stack of the game
+        virtual void update(std::vector<std::shared_ptr<IEntity>> &entities, std::stack<Arcade::Evt> &events) = 0;
 
         /// \brief Get the game's status
         /// \return the game's status
-        /// \note Calling this method without calling IGame::setup() beforehand will result in <b>undefined behavior</b>
         virtual GameState getState() const = 0;
 
         /// \brief starts the game
-        /// \return true if the game was successfully started
-        /// \note Calling this method without calling IGame::setup() beforehand will result in <b>undefined behavior</b>
-        virtual bool start() = 0;
+        virtual void start() = 0;
 };
 
 

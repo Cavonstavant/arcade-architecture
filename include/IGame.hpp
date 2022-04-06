3a1889da777ca6d677b2d4356e49825e28dfb206
Restyled.io <commits@restyled.io>
2022-04-06 22:02:55 +0000

    Restyled by astyle

--- include/IGame.hpp
+++ include/IGame.hpp
@@ -19,39 +19,39 @@
 
 /// Game instance
 class IGame {
-    public:
-        /// Represents the game's status
-        enum class GameState {
-            /// Represents the game at it's initial state
-            LOADED,
-            /// Represents the game when running
-            RUNNING,
-            /// Represents the previously running game is paused
-            PAUSED,
-            /// Represents the game over
-            STOPPED,
-        };
+public:
+    /// Represents the game's status
+    enum class GameState {
+        /// Represents the game at it's initial state
+        LOADED,
+        /// Represents the game when running
+        RUNNING,
+        /// Represents the previously running game is paused
+        PAUSED,
+        /// Represents the game over
+        STOPPED,
+    };
 
-        virtual ~IGame() = default;
+    virtual ~IGame() = default;
 
-        /// Initialize the game
-        virtual void init(std::vector<std::shared_ptr<IEntity>> &entities) = 0;
+    /// Initialize the game
+    virtual void init(std::vector<std::shared_ptr<IEntity>> &entities) = 0;
 
-        /// \brief Update the game's status
-        /// \param entities The entity vector of the game
-        /// \param events The event stack of the game
-        virtual void update(std::vector<std::shared_ptr<IEntity>> &entities,
-                            std::stack<Arcade::Evt> &events) = 0;
+    /// \brief Update the game's status
+    /// \param entities The entity vector of the game
+    /// \param events The event stack of the game
+    virtual void update(std::vector<std::shared_ptr<IEntity>> &entities,
+                        std::stack<Arcade::Evt> &events) = 0;
 
-        /// \brief Get the game's status
-        /// \return the game's status
-        virtual GameState getState() const = 0;
+    /// \brief Get the game's status
+    /// \return the game's status
+    virtual GameState getState() const = 0;
 
-        /// \brief Get the game's name
-        virtual std::string getLibraryName() const = 0;
+    /// \brief Get the game's name
+    virtual std::string getLibraryName() const = 0;
 
-        /// \brief starts the game
-        virtual void start() = 0;
+    /// \brief starts the game
+    virtual void start() = 0;
 };
 
 #endif// ARCADE_IGAME_HPP
